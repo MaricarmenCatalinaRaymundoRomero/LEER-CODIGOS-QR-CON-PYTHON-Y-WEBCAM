ad51f9c12ce4c5a9a1085d753e92b82a2586d199
Maricarmen Catalina Raymundo Romero <129924045+MaricarmenCatalinaRaymundoRomero@users.noreply.github.com>
2024-01-02 22:46:00 -0500

    Update generar_qr.py

--- generar_qr.py
+++ generar_qr.py
@@ -1,4 +1,3 @@
-
 """
 PASOS PREVIOS.
 
@@ -12,13 +11,19 @@ pip install pandas
 
 import qrcode
 import pandas as pd
-#LEEMOS LA CANTIDAD DE DATOS DE NUESTRA BASE
+
+# LEEMOS LA CANTIDAD DE DATOS DE NUESTRA BASE
 data = pd.read_csv('BASE.csv')
 print(data.head())
 
+# Iteramos sobre cada fila de los datos
 for i in range(len(data)):
-    cod_proveedor = data.iloc[i,0]
-    nombre_proveedor = data.iloc[i,1]
+    # Obtenemos el código del proveedor y su nombre
+    cod_proveedor = data.iloc[i, 0]
+    nombre_proveedor = data.iloc[i, 1]
     
+    # Creamos un código QR con el código del proveedor
     img = qrcode.make(cod_proveedor)
+    
+    # Guardamos la imagen con el nombre del proveedor en la carpeta 'img'
     img.save(f"img/{nombre_proveedor}.png")
